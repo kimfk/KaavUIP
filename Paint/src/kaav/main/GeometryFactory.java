c991fa80391fb76870f1dc147c387fb8d09c8a66
Ganryu <Ganryu@192.168.0.2>
2014-04-13 22:53:24 +0200

    Added a very incomplete uml diagram. KAAV.dia

--- Paint/src/kaav/main/GeometryFactory.java
+++ Paint/src/kaav/main/GeometryFactory.java
@@ -1,6 +1,15 @@
 package kaav.main;
 
+/**
+ * This class is used to create new instances of Drawables.
+ * @author Ganryu
+ *
+ */
 public class GeometryFactory {
+	/**
+	 * Create a new Rectangle
+	 * @return
+	 */
 	public Drawable makeRectangle(){
 		Rectangle c = new Rectangle();
 		return c;
