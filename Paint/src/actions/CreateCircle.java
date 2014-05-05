de091cbcea94b6cf5cc18041a1eaca7df02d6fb1
Ganryu <Ganryu@192.168.0.2>
2014-05-05 02:15:44 +0200

    Added some GActions in the .actions package. They contain no functionality currently. They will need to make appropriate method calls.

---
+++ Paint/src/actions/CreateCircle.java
@@ -0,0 +1,10 @@
+package actions;
+
+import kaav.main.GAction;
+
+public class CreateCircle implements GAction{
+	@Override
+	public void act() {
+		// TODO call methods here
+	}
+}
---
+++ Paint/src/actions/CreateSquare.java
@@ -0,0 +1,10 @@
+package actions;
+
+import kaav.main.GAction;
+
+public class CreateSquare implements GAction{
+	@Override
+	public void act() {
+		// TODO call methods here
+	}
+}
