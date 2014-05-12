c013dbf0e2d997afd83d5abea63a74ed15047c23
kimfk <kimfeenstrakuiper@gmail.com>
2014-05-12 12:39:52 +0200

    detecting 2 fingers improved

--- Paint/src/leapMotion/LeapMotionDetectsMovement.java
+++ Paint/src/leapMotion/LeapMotionDetectsMovement.java
@@ -48,23 +48,25 @@ class LeapMotionDetectsMovement extends Listener {
     public void onFrame(Controller controller) {
         // Get the most recent frame and report some basic information
         Frame frame = controller.frame();
-        int amount_fingers = frame.fingers().count();
+        
         int amount_hands = frame.hands().count();
-        Vector position =  frame.fingers().get(0).tipPosition();
         int simplevectorx, simplevectory;
         Vector temporary;
         
         if (amount_hands == 1){
+        	int amount_fingers = frame.fingers().count();
+        	Vector position1 =  frame.fingers().get(0).tipPosition();
+        	Vector position2 =  frame.fingers().get(1).tipPosition();
         	if (amount_fingers > 0){
 		    	//Amount fingers = 1 --> Free drawing
 		    	if(amount_fingers == 1){
-		    		if(position.getZ() < 0) {
+		    		if(position1.getZ() < 0) {
 		    		temporary = frame.interactionBox().normalizePoint(frame.fingers().get(0).tipPosition());
 		    		coordinateslist.add(new SimpleVector(temporary.getX(), temporary.getY()));
 		    		System.out.println("- Z, add to the list : " + frame.fingers().get(0).tipPosition());
 		    		//coordinateslist.add(frame.fingers().get(0).tipPosition());
 		    	}
-		    	else if((position.getZ()>0) && !coordinateslist.isEmpty()){
+		    	else if((position1.getZ()>0) && !coordinateslist.isEmpty()){
 		    	    System.out.println("Now i will send the list to andreas: " + coordinateslist.toString());
 		    	    gesturizer.compare(coordinateslist);
 		    		coordinateslist.clear();
@@ -72,7 +74,7 @@ class LeapMotionDetectsMovement extends Listener {
 		    }
 		    	
 	    	//amount fingers = 2 --> replacing
-	    	else if(amount_fingers == 2 && position.getZ() < 0){
+	    	else if(amount_fingers == 2 && position1.getZ() < 0 && position2.getZ() <0){
 	    		System.out.println("Drag item to new position: " +frame.fingers().get(0).tipPosition());
 	        	//send new coordinates to andreas 
 	        	//coordinateslist.clear();		
